188327541395bde1d25620b49115b3719125419a
Quentin Smith <quentin@mit.edu>
2022-07-08 03:36:21 -0400

    Handle NaN in max

--- discobiker/src/output.rs
+++ discobiker/src/output.rs
@@ -22,8 +22,10 @@ impl CalculateIlluminance for LightData {
 pub fn max<T: PartialOrd>(a: T, b: T) -> T {
     if a > b {
         a
-    } else {
+    } else if b == b {
         b
+    } else {
+        a
     }
 }
 
