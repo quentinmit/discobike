a30aec48ff8fa09fa960705df976699462102538
Quentin Smith <quentin@mit.edu>
2022-07-11 01:48:51 -0400

    Start to write text to the display

--- discobiker/src/actors/display.rs
+++ discobiker/src/actors/display.rs
@@ -1,7 +1,14 @@
 use defmt::*;
 use core::fmt;
+use arrayvec::ArrayString;
+use core::fmt::Write;
 use embassy::time::{Duration, Timer};
 use embedded_graphics::prelude::*;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    text::{Baseline, Text},
+};
 
 use embedded_hal_async::i2c;
 
@@ -20,6 +27,24 @@ pub enum DisplayMessage {
     Off,
 }
 
+#[derive(Debug)]
+pub enum Error {
+    DisplayError(DisplayError),
+    FormatError(fmt::Error),
+}
+
+impl From<DisplayError> for Error {
+    fn from(e: DisplayError) -> Self {
+        Self::DisplayError(e)
+    }
+}
+
+impl From<fmt::Error> for Error {
+    fn from(e: fmt::Error) -> Self {
+        Self::FormatError(e)
+    }
+}
+
 // DisplaySize128x64
 impl<I2C, E, SIZE> Display<I2C, SIZE>
 where
@@ -34,10 +59,25 @@ where
         Self { display }
     }
 
-    async fn run_display<M>(&mut self, mut inbox: &M) -> Result<(), DisplayError>
+    async fn run_display<M>(&mut self, mut inbox: &M) -> Result<(), Error>
     where M: Inbox<DisplayMessage>
     {
         self.display.init().await?;
+        let text_style = MonoTextStyleBuilder::new()
+            .font(&FONT_6X10)
+            .text_color(BinaryColor::On)
+            .build();
+        const COLS: usize = 128/6;
+        let mut buf = ArrayString::<COLS>::new();
+        // Line 0: XX.XVext X.XXXA X.XXV
+        core::write!(&mut buf, "{:04.1}Vext {:05.3}A {:04.2}{}", 12.1, 0.322, 4.01, "V")?;
+        Text::with_baseline(&buf, Point::zero(), text_style, Baseline::Top)
+            .draw(&mut self.display)?;
+        // Line 1: XXX°XXX° XXX.X°
+        // Line 2: XXXX.XXhPa XXX.XX'
+        // Line 3: XXX°TXXX.XX°
+        // Line 4: XXX.XXG    XXXXX lux
+        // Line 5: Mode: Day XXX% XXs
         Ok(())
     }
 }
