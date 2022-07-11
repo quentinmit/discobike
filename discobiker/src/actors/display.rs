6ba6a6c3af52d88ff9b1b98a5ce2abc0043a621f
Quentin Smith <quentin@mit.edu>
2022-07-11 01:53:24 -0400

    rustfmt

--- discobiker/src/actors/display.rs
+++ discobiker/src/actors/display.rs
@@ -1,7 +1,7 @@
-use defmt::*;
-use core::fmt;
 use arrayvec::ArrayString;
+use core::fmt;
 use core::fmt::Write;
+use defmt::*;
 use embassy::time::{Duration, Timer};
 use embedded_graphics::prelude::*;
 use embedded_graphics::{
@@ -12,14 +12,16 @@ use embedded_graphics::{
 
 use embedded_hal_async::i2c;
 
-use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306, size::DisplaySize, mode::BufferedGraphicsMode};
 use display_interface::DisplayError;
+use ssd1306::{
+    mode::BufferedGraphicsMode, prelude::*, size::DisplaySize, I2CDisplayInterface, Ssd1306,
+};
 
 use ector::{actor, Actor, Address, Inbox};
 
 pub struct Display<I2C, SIZE: DisplaySize> {
     // interface, mode, size, addr_mode, rotation
-    display: Ssd1306<I2CInterface<I2C>, SIZE, BufferedGraphicsMode<SIZE>>
+    display: Ssd1306<I2CInterface<I2C>, SIZE, BufferedGraphicsMode<SIZE>>,
 }
 
 pub enum DisplayMessage {
@@ -54,23 +56,31 @@ where
     pub fn new(i2c: I2C, size: SIZE) -> Self {
         let interface = I2CDisplayInterface::new(i2c);
 
-        let mut display = Ssd1306::new(interface, size, DisplayRotation::Rotate180)
-            .into_buffered_graphics_mode();
+        let mut display =
+            Ssd1306::new(interface, size, DisplayRotation::Rotate180).into_buffered_graphics_mode();
         Self { display }
     }
 
     async fn run_display<M>(&mut self, mut inbox: &M) -> Result<(), Error>
-    where M: Inbox<DisplayMessage>
+    where
+        M: Inbox<DisplayMessage>,
     {
         self.display.init().await?;
         let text_style = MonoTextStyleBuilder::new()
             .font(&FONT_6X10)
             .text_color(BinaryColor::On)
             .build();
-        const COLS: usize = 128/6;
+        const COLS: usize = 128 / 6;
         let mut buf = ArrayString::<COLS>::new();
         // Line 0: XX.XVext X.XXXA X.XXV
-        core::write!(&mut buf, "{:04.1}Vext {:05.3}A {:04.2}{}", 12.1, 0.322, 4.01, "V")?;
+        core::write!(
+            &mut buf,
+            "{:04.1}Vext {:05.3}A {:04.2}{}",
+            12.1,
+            0.322,
+            4.01,
+            "V"
+        )?;
         Text::with_baseline(&buf, Point::zero(), text_style, Baseline::Top)
             .draw(&mut self.display)?;
         // Line 1: XXX°XXX° XXX.X°
@@ -92,7 +102,8 @@ where
     type Message<'m> = DisplayMessage;
 
     async fn on_mount<M>(&mut self, _: Address<DisplayMessage>, mut inbox: M)
-    where M: Inbox<DisplayMessage>
+    where
+        M: Inbox<DisplayMessage>,
     {
         loop {
             if let Err(e) = self.run_display(&mut inbox).await {
