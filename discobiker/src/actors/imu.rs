0ad670cda91630fdd0103fd2b00506daacbb8e11
Quentin Smith <quentin@mit.edu>
2022-07-16 16:15:55 -0400

    Update state with acceleration

--- discobiker/src/actors/imu.rs
+++ discobiker/src/actors/imu.rs
@@ -1,20 +1,19 @@
-use crate::{STATE, LSM6DS33_ADDR};
+use crate::{LSM6DS33_ADDR, STATE};
 use core::fmt;
 use defmt::*;
-use lsm6ds33::{self, *};
-use ector::{actor, Actor, Address, Inbox};
-use embedded_hal_async::i2c;
-use embassy::time::{Duration, Timer};
 use dim::derived;
-use dim::si::{
-    f32consts::{MPS2},
-    MeterPerSecond2,
-};
+use dim::{typenum::Pow, Sqrt};
+use dim::typenum::P2;
+use dim::si::{f32consts::MPS2, MeterPerSecond2};
 use dim::ucum::{
-    self, UCUM,
+    self,
     f32consts::{DEG, RAD, S},
-    Radian, Second
+    Radian, Second, UCUM,
 };
+use ector::{actor, Actor, Address, Inbox};
+use embassy::time::{Duration, Timer};
+use embedded_hal_async::i2c;
+use lsm6ds33::{self, *};
 
 derived!(ucum, UCUM: RadianPerSecond = Radian / Second);
 //const RADPS: RadianPerSecond<f32> = RAD / S;
@@ -23,8 +22,7 @@ pub struct Imu<I2C> {
     lsm6ds33: Lsm6ds33<I2C>,
 }
 
-pub enum ImuMessage {
-}
+pub enum ImuMessage {}
 
 #[derive(Debug)]
 pub enum Error<I2C>
@@ -32,7 +30,7 @@ where
     I2C: i2c::I2c,
 {
     I2c(I2C::Error),
-    Lsm6ds33(lsm6ds33::Error<I2C::Error>)
+    Lsm6ds33(lsm6ds33::Error<I2C::Error>),
 }
 
 // impl<I2C, E> From<E> for Error<I2C>
@@ -59,31 +57,56 @@ where
 {
     pub async fn new(i2c: I2C) -> Result<Self, Error<I2C>> {
         let lsm6ds33 = Lsm6ds33::new(i2c, LSM6DS33_ADDR).await?;
-        Ok(Imu {
-            lsm6ds33
-        })
+        Ok(Imu { lsm6ds33 })
     }
     async fn run_imu<M>(&mut self, _inbox: &mut M) -> Result<(), Error<I2C>>
     where
         M: Inbox<ImuMessage>,
     {
         info!("initializing imu");
-        self.lsm6ds33.set_accelerometer_scale(AccelerometerScale::G02).await?;
-        self.lsm6ds33.set_gyroscope_scale(GyroscopeFullScale::Dps245).await?;
-        self.lsm6ds33.set_accelerometer_output(AccelerometerOutput::Rate104).await?;
-        self.lsm6ds33.set_gyroscope_output(GyroscopeOutput::Rate104).await?;
-        self.lsm6ds33.set_accelerometer_bandwidth(AccelerometerBandwidth::Freq400).await?;
+        self.lsm6ds33
+            .set_accelerometer_scale(AccelerometerScale::G02)
+            .await?;
+        self.lsm6ds33
+            .set_gyroscope_scale(GyroscopeFullScale::Dps245)
+            .await?;
+        self.lsm6ds33
+            .set_accelerometer_output(AccelerometerOutput::Rate104)
+            .await?;
+        self.lsm6ds33
+            .set_gyroscope_output(GyroscopeOutput::Rate104)
+            .await?;
+        self.lsm6ds33
+            .set_accelerometer_bandwidth(AccelerometerBandwidth::Freq400)
+            .await?;
         self.lsm6ds33.set_low_power_mode(true).await?;
         loop {
             let accel_raw = self.lsm6ds33.read_accelerometer().await?; // m/s^2
-            let accel: [MeterPerSecond2<f32>; 3] = [accel_raw.0*MPS2, accel_raw.1*MPS2, accel_raw.2*MPS2];
+            let accel: [MeterPerSecond2<f32>; 3] =
+                [accel_raw.0 * MPS2, accel_raw.1 * MPS2, accel_raw.2 * MPS2];
             let gyro_raw = self.lsm6ds33.read_gyro().await?; // rads/s
-            let gyro: [RadianPerSecond<f32>; 3] = [gyro_raw.0*RAD/S, gyro_raw.1*RAD/S, gyro_raw.2*RAD/S];
-            Timer::after(Duration::from_millis(1000/50)).await;
+            let gyro: [RadianPerSecond<f32>; 3] = [
+                gyro_raw.0 * RAD / S,
+                gyro_raw.1 * RAD / S,
+                gyro_raw.2 * RAD / S,
+            ];
+            let accel_mag =
+                (accel[0].powi(P2::new()) + accel[1].powi(P2::new()) + accel[2].powi(P2::new())).sqrt();
+
+            STATE.lock(|c| {
+                c.update(|s| {
+                    let mut s = s;
+                    s.accel_mag = Some(accel_mag);
+                    if s.accel_mag.map_or(false, |v| v > 12.0*MPS2) {
+                        s.move_timer.update();
+                    }
+                    s
+                })
+            });
+            Timer::after(Duration::from_millis(1000 / 50)).await;
         }
     }
 }
-
 
 #[actor]
 impl<I2C, E> Actor for Imu<I2C>
