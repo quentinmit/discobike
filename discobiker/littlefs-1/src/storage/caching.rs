cd200ef545c93f1a96152348a3326a4354ced552
Quentin Smith <quentin@mit.edu>
2022-07-29 17:22:00 -0400

    More implementation for caching

--- discobiker/littlefs-1/src/storage/caching.rs
+++ discobiker/littlefs-1/src/storage/caching.rs
@@ -18,6 +18,26 @@ impl<const BLOCK_SIZE: usize> AddressedBlock<BLOCK_SIZE> {
     }
 }
 
+trait Rounding {
+    fn last_multiple_of(&self, rhs: Self) -> Self;
+    fn next_multiple_of(&self, rhs: Self) -> Self;
+}
+
+impl<T> Rounding for T
+where T: Rem<Output = T> + Sub<Output = T> + Add<Output = T> + Default + PartialEq + Copy
+{
+    fn last_multiple_of(&self, rhs: Self) -> Self {
+        *self - (*self % rhs)
+    }
+    fn next_multiple_of(&self, rhs: Self) -> Self {
+        if *self % rhs != Default::default() {
+            *self + rhs - (*self % rhs)
+        } else {
+            *self
+        }
+    }
+}
+
 #[cfg(feature = "async")]
 pub struct CachingStorage<T>
     where T: AsyncNorFlash
@@ -36,6 +56,49 @@ impl CachingStorage<T>
             cache: None,
         }
     }
+    async fn commit(&mut self) -> Result<(), NorFlashErrorKind> {
+        match self.cache.take() {
+            Some(block) => {
+                self.storage.erase(block.start, block.start+data.len() as u32).await?;
+                self.storage.write(block.start, block.buf.as_slice()).await?;
+            }
+            None => ()
+        }
+        Ok(())
+    }
+    async fn fetch_block_containing(&mut self, offset: u32) -> Result<(), NorFlashErrorKind> {
+        match self.cache {
+            Some(block) => {
+                if !block.contains(offset) {
+                    self.commit().await?;
+                }
+            },
+            None => (),
+        }
+        match self.cache {
+            None => {
+                let start = offset.last_multiple_of(T::ERASE_SIZE);
+                let buf = Block::<T::ERASE_SIZE>::zero(T::ERASE_SIZE);
+                self.storage.read(start, buf.as_mut_slice()).await?;
+                self.cache = Some(AddressedBlock{
+                    start,
+                    buf
+                })
+            },
+            Some(_) => (),
+        }
+    }
+    async fn write_partial(&mut self, offset: u32, data: &[u8]) -> Result<(), NorFlashErrorKind> {
+        self.fetch_block_containing(offset).await?;
+        match self.cache.as_mut() {
+            None => panic!("failed to fetch block"),
+            Some(block) => {
+                let start = offset - block.start;
+                block.buf.as_mut_slice()[start..start+data.len()].copy_from(data);
+            }
+        }
+        Ok(())
+    }
 }
 
 trait Split<T>
@@ -46,19 +109,17 @@ trait Split<T>
 }
 
 impl<T> Split<T> for Range<T>
-    where T: Rem<Output = T> + Sub<Output = T> + AddAssign + SubAssign + PartialEq + PartialOrd + Default + Copy
+    where T: Rem<Output = T> + Sub<Output = T> + Add<Output = T> + AddAssign + SubAssign + PartialEq + PartialOrd + Default + Copy
 {
     fn split_partials(&self, block_size: T) -> (Option<Self>, Option<Self>, Option<Self>) {
-        let mut start = self.start;
-        let prefix = if self.start % block_size != Default::default() {
-            start += block_size - (self.start % block_size);
+        let mut start = self.start.next_multiple_of(block_size);
+        let prefix = if self.start != start {
             Some(self.start .. start)
         } else {
             None
         };
-        let mut end = self.end;
-        let suffix = if self.end % block_size != Default::default() {
-            end -= self.end % block_size;
+        let mut end = self.end.last_multiple_of(block_size);
+        let suffix = if self.end != end {
             Some(end .. self.end )
         } else {
             None
@@ -88,7 +149,7 @@ impl<T: AsyncNorFlash> AsyncNorFlash for CachingStorage<T> {
             let range = (offset..offset+(data.len() as u32));
             let (prefix, infix, suffix) = range.split_partials(T::ERASE_SIZE);
             if let Some(prefix) = prefix {
-                self.write_partial(prefix, &data[0..prefix.len()]).await?;
+                self.write_partial(prefix.start, &data[0..prefix.len()]).await?;
             }
             if let Some(infix) = infix {
                 if self.cache.map_or(false, |c| c.contains(infix.start)) {
@@ -98,7 +159,7 @@ impl<T: AsyncNorFlash> AsyncNorFlash for CachingStorage<T> {
                 self.storage.write(offset, &data[infix.sub(offset)]).await?;
             }
             if let Some(suffix) = suffix {
-                self.write_partial(prefix, &data[0..prefix.len()]).await?;
+                self.write_partial(prefix.start, &data[0..prefix.len()]).await?;
             }
         }
     }
